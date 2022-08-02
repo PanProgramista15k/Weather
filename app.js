cf630001664eb3abd93d958962574c0703db0543
PanProgramista15k <traningcompany@yahoo.com>
2022-08-02 16:29:09 +0200

    chicago ver. 0.2

--- app.js
+++ app.js
@@ -4,28 +4,33 @@ const celsius = Math.floor(Math.random() * max) + min;
 const fahrenheit = Math.floor(celsius * (9 / 5) + 32);
 console.log(celsius);
 console.log(fahrenheit);
+
 if (celsius > 30) {
-	console.log(
-		"Be careful! High temperature might be dangerous for your health"
-	);
+	msg = "Be careful! High temperature might be dangerous for your health";
 } else if (celsius > 24) {
-	console.log("Enjoy hot summer temperature! Stay hydrated");
+	msg = "Enjoy hot summer temperature! Stay hydrated";
 } else if (celsius > 21) {
-	console.log("Ahhh! Perfect temperature! Not to warm, not to cold");
+	msg = "Ahhh! Perfect temperature! Not to warm, not to cold";
 } else if (celsius > 15) {
-	console.log(
-		"Great temperture for a long walk! Remember about 10.000 steps"
-	);
+	msg = "Great temperture for a long walk! Remember about 10.000 steps";
 } else if (celsius > 10) {
-	console.log("Good temperture for a short walk! But take sweater");
+	msg = "Good temperture for a short walk! But take sweater";
 } else if (celsius > 0) {
-	console.log("Don't forget your cap! ");
+	msg = "Don't forget your cap! ";
 } else if (celsius > -5) {
-	console.log("Wear warm coat and scarf! Watch out for ice");
+	msg = "Wear warm coat and scarf! Watch out for ice";
 } else if (celsius > -20) {
-	console.log(
-		"Brr! There is freezing! Gloves are important to protect your fingers before frostbite"
-	);
+	msg =
+		"Brr! There is freezing! Gloves are important to protect your fingers before frostbite";
 } else {
-	console.log("Invalid temperature");
+	msg = "Invalid temperature";
 }
+
+const a = document.querySelector(".msg");
+a.innerText = msg;
+
+const cel = document.querySelector("#celsius");
+cel.innerText = celsius;
+
+const fah = document.querySelector("#fahrenheit");
+fah.innerText = fahrenheit;
